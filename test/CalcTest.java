453ac85f1198c90b0f7b262d2a2af570b7d404d8
Tóth Fedor László <toth.fedor@gmail.com>
2023-05-23 18:07:34 +0200

    Fix2

--- test/CalcTest.java
+++ test/CalcTest.java
@@ -1,18 +1,20 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+/*
+File: CalcTest.java
+Author: Tóth Fedor László, SzoftI2E
+Copyright: 2023, Tóth Fedor László
+Date: 2023.05.23
+Web: https://github.com/TothFL
+*/
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
     MainFrame mainFrame;
-
-    @BeforeEach
-    public void initEach() {
-        this.mainFrame = new MainFrame();
-    }
     
     @Test
     public void testCalTerfogat() {
+        this.mainFrame = new MainFrame();
         double actual = this.mainFrame.calTerfogat(5, 4,8);
         double expected = 160;
         assertEquals(expected, actual, 0.1);
--- test/InputTest.java
+++ test/InputTest.java
@@ -1,24 +1,28 @@
+/*
+File: InputTest.java
+Author: Tóth Fedor László, SzoftI2E
+Copyright: 2023, Tóth Fedor László
+Date: 2023.05.23
+Web: https://github.com/TothFL
+*/
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InputTest {
     MainFrame mainFrame;
 
-    @BeforeEach
-    public void initEach() {
-        this.mainFrame = new MainFrame();
-    }
-
     @Test
     public void testCheckInputBetu() {
+        this.mainFrame = new MainFrame();
         boolean actual = this.mainFrame.checkInput("f");
         assertFalse(actual);
     }
     @Test
     public void testCheckInputSzam() {
+        this.mainFrame = new MainFrame();
         boolean actual = this.mainFrame.checkInput("64981968");
         assertTrue(actual);
     }
